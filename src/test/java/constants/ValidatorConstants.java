41b276c974030b47cfeeafdf718a694eeebcf564
RomanKamko <r.kamko@mail.ru>
2023-05-15 18:21:54 +0400

    - написал тест для ValidatorServiceImpl

---
+++ src/test/java/constants/ValidatorConstants.java
@@ -0,0 +1,11 @@
+package constants;
+
+public class ValidatorConstants {
+    public static final String GOOD_INPUT = "Ivan";
+    public static final String BAD_INPUT_1 = " Ivan";
+    public static final String BAD_INPUT_2 = "I_van";
+    public static final String BAD_INPUT_3 = "Iv-an";
+    public static final String BAD_INPUT_4 = "Ivan1";
+    public static final String BAD_INPUT_5 = "";
+
+}
